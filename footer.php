5a1408bcadd2e8f1676126fed04206a65fd47734
Tgemayel <toni@twogiraffes.com>
2013-04-28 19:24:02 -0400

    Adding gauges

--- footer.php
+++ footer.php
@@ -3,5 +3,21 @@
     <p><?php echo site_name(); ?>. - Running on <a href="http://anchorcms.com">Anchor</a>, source on <a href="https://github.com/Tgemayel">Github</a>.</p>
     </section>
   </footer>
+
+  <script type="text/javascript">
+  var _gauges = _gauges || [];
+  (function() {
+    var t   = document.createElement('script');
+    t.type  = 'text/javascript';
+    t.async = true;
+    t.id    = 'gauges-tracker';
+    t.setAttribute('data-site-id', '4e838007613f5d659c000001');
+    t.src = '//secure.gaug.es/track.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(t, s);
+  })();
+</script>
+
+
 </body>
 </html>
